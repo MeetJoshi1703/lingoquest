c41b62969ccf182640573395040465d9e5edda6d
MeetJoshi1703 <98758359+MeetJoshi1703@users.noreply.github.com>
2024-01-18 17:38:12 +0530

    homescreen link change

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector} from 'react-redux';
 
 const HomeScreen = () => {
+
+  const { userInfo } = useSelector((state) => state.auth);
+
   return (
     <>
       <h1>Welcome to the Language Learning Game</h1>
-      <p>Login to play the game. you can <Link to="/login">login here</Link>.</p>
+      {userInfo?(
+        <p>Select which Language you want to learn <Link to="/select-language"> here</Link></p>
+      ):(
+        <p>Login to play the game. you can <Link to="/login">login here</Link>.</p>
+      )}
+      
     </>
   );
 };
